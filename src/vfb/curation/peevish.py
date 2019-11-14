63a9d7dd94bfa77ee61f7f4f617f90efb68acb3d
dosumis <dosumis@gmail.com>
2019-11-14 17:44:19 +0000

    First working version with curation record testing.

--- src/vfb/curation/peevish.py
+++ src/vfb/curation/peevish.py
@@ -2,38 +2,101 @@
 
 import pandas as pd
 import yaml
+import ruamel_yaml
 import glob
 import re
+from collections import namedtuple, Counter
+import warnings
+import os
 
 
-def strip_file_extension(ext, filename):
-    m = re.search("^(.+)\." + ext + "$", "filename")
-    return m.group(1)
+
+# This is built with the assumption that all curation involves
+# (a) Paired YAML &
+# (b) Spec file defined by location - not name.
+## But latest draft spec has file types defined by name.
+## Managing this would require a more complicated architecture.
+## Compromise:
+# 1. Separate space for extending annotations on existing Inds.
+# 2. Names of spec files used are driven by Type prefix
+
+# from dataclasses import dataclass, field
+#
+# @dataclass
+# class CurFile:
+#     loc: str
+#     name: str
+#     ext: str
+#     type: str
+#     dataset: str
+#     date: str
+
+
+
+# def strip_file_extension(ext, filename):
+#
+#     m = re.search("(.+)/(.+)\.(.+)$" + ext + "$", "filename")
+#     if m:
+#         return m.group(1)
+#     else:
+#         return False
+
+CurFile = namedtuple('CurFile', ['path', 'loc', 'extended_name',
+                                 'name', 'ext', 'type',
+                                 'dataset', 'date'])
+
+def process_file_path(file_path):
+    path = file_path.split('/')
+    extended_name = path[-1]
+    name_ext = extended_name.split('.')
+    if not (len(name_ext) == 2):
+        warnings.warn("Can't parse filename extension for %s"
+                      "Wrong number of '.'" % extended_name)
+        return False
+    name_breakdown = name_ext[0].split('_')
+    if not (len(name_breakdown) == 3):
+        warnings.warn("Curation FileName (%s) has wrong format,"
+                      "should be: type_dataset_date.ext" % extended_name)
+        return False
+    return CurFile(path=file_path,
+                   loc=path[0],
+                   extended_name=extended_name,
+                   name=name_ext[0],
+                   ext=name_ext[1],
+                   type=name_breakdown[0],
+                   dataset=name_breakdown[1],
+                   date=name_breakdown[2]
+                   )
+
 
 def get_recs(path_to_recs, spec_path):
     # Check _ Does glob preserve path?
-    tsv_recs = glob.glob(path_to_recs + "*.tsv")
-    yaml_recs = glob.glob(path_to_recs + "*.yaml")
+    # Assumes pairs of yaml+tsv files with same name apart from extension.
+    # Generates record by combining the two.
 
-    rec_names_from_yaml = [strip_file_extension("yaml", r) for r in yaml_recs]
-    rec_names_from_tsv = [strip_file_extension("tsv", r) for r in tsv_recs]
+    tsv_rec_files = glob.glob(path_to_recs + "*.tsv")
+    yaml_rec_files = glob.glob(path_to_recs + "*.yaml")
+    tsv_recs = [process_file_path(r) for r in tsv_rec_files]
+    yaml_recs = [process_file_path(r) for r in yaml_rec_files]
 
-    try: assert set(rec_names_from_yaml) == set(rec_names_from_tsv)
-    except ValueError:
-        print("Non matching records.")
-        yaml_not_tsv = set(rec_names_from_yaml) - set(rec_names_from_tsv)
-        if yaml_not_tsv:
-            print("Yaml files without matching tsv %s" % str(yaml_not_tsv))
-        tsv_not_yaml = set(rec_names_from_tsv) - set(rec_names_from_yaml)
-        if tsv_not_yaml:
-            print("tsv files without matching yaml %s" % str(tsv_not_yaml))
+    # Find pairs
+
+    combined_recs = []
+    if yaml_recs:
+        yaml_rec_lookup = {y.name: y for y in yaml_recs}
     else:
-        rec_names = rec_names_from_tsv
-        records = {}
-        for r in rec_names:
-            records[r] = Record(spec_path= spec_path,
-                                 tsv_path= r + ".tsv",
-                                 yaml_path= r + "*.yaml")
+        yaml_rec_lookup = {}
+
+    for t in tsv_recs:
+            if t.name in yaml_rec_lookup.keys():
+                combined_recs.append((t, yaml_rec_lookup[t.name]))
+            else:
+                combined_recs.append((t,))
+
+    records = []
+    for r in combined_recs:
+        records.append(Record(spec_path=spec_path,
+                            cur_recs=r))
 
     return records
 
@@ -43,20 +106,27 @@ def get_recs(path_to_recs, spec_path):
 
 class Record(object):
 
-    def __init__(self, spec_path, tsv_path, yaml_path):
-        """path to tsv"""
+    def __init__(self, spec_path, cur_recs):
 
-        specfile = open(spec_path, "r")
-        self.spec = yaml.load(specfile.read())
-        specfile.close()
-        self.tsv = pd.DataFrame.from_csv(tsv_path, sep = "\t")
-        yfile = open(yaml_path, 'r')
-        self.y = yaml.load(yfile.read())
+        """path to tsv"""
+        with open(spec_path + cur_recs[0].type + '_spec.yaml', 'r') as type_spec_file:
+            with open(spec_path + 'common_fields_spec.yaml', 'r') as general_spec_file:
+                self.spec = ruamel_yaml.safe_load(type_spec_file.read())
+                general_spec = ruamel_yaml.safe_load(general_spec_file.read())
+                self.spec.update(general_spec)
+
+        for cr in cur_recs:
+            if cr.ext == 'tsv':
+                self.tsv = pd.read_csv(cr.path, sep="\t")
+            elif cr.ext == 'yaml':
+                y_file = open(cr.path, 'r')
+                self.y = ruamel_yaml.safe_load(y_file.read())
+            else:
+                warnings.warn("Unknown file type %s" % cr.extended_name)
 
     def check_working(self):
         self.check_tsv_headers()
         self.check_uniqueness()
-        self.check_yaml_keys()
 
     def check_all(self):
         self.check_working()
@@ -65,13 +135,15 @@ class Record(object):
 
 # Should probably refactor to a single key check function.
     def check_tsv_headers(self):
-        input_columns = set(self.tsv.Columns)
+        input_columns = set(self.tsv.columns).union(set(self.y.keys()))
         # Allow for columns that are legal in YAML to be present in tsv.
         spec_columns = set(self.spec.keys())
-        compulsory_columns = set([k for k in self.spec.keys if k['Compulsory']])
+        compulsory_columns = set([k for k, v in
+                                  self.spec.items() if v['compulsory']])
 
+        #  Check all input columns in spec
         try:
-            assert input_columns == spec_columns
+            assert not (input_columns - spec_columns)
         except ValueError:
             illegal_columns = input_columns - spec_columns
             print("TSV contains illegal columns: %s" % str(illegal_columns))
@@ -80,34 +152,18 @@ class Record(object):
             assert input_columns.issuperset(compulsory_columns)
         except ValueError:
             print("TSV is missing compulsory columns: %s" %
-                    str(compulsory_columns - spec_columns))
+                  str(compulsory_columns - spec_columns))
 
-    def check_yaml_keys(self):
-        input_columns = set(self.y.keys())
-        spec_columns = set([k for k in self.spec.keys()
-                            if self.spec[k]['yaml']])
-        compulsory_columns = set([k for k in self.spec.keys if k['Compulsory']])
-
-        try:
-            assert input_columns == spec_columns
-        except ValueError:
-            illegal_columns = input_columns - spec_columns
-            print("YAML contains illegal keys: %s" % str(illegal_columns))
-
-        try:
-            assert input_columns.issuperset(compulsory_columns)
-        except ValueError:
-            print("YAML is missing compulsory keys: %s" %
-                    str(compulsory_columns - spec_columns))
 
     def check_uniqueness(self):
-        uniq_columns = [k for k in self.spec.keys if k['is_unique']]
+        uniq_columns = [k for k, v in self.spec.items() if 'uniq' in v.keys() and v['uniq']]
         for c in uniq_columns:
-            contents = list(self.tsv[c])
-            s = set()
-            duplicates = set(x for x in contents if x in s or s.add(x))
+            # Need to strip Nan - to cope with empty rows!
+            contents = list(self.tsv[c].dropna())
+            duplicates = [item for item, count
+                          in Counter(contents).items() if count > 1]
             try:
-                assert duplicates is False
+                assert len(duplicates) == 0
             except ValueError:
                 print("Entries in Column %s should be uniq, but duplicate "
                       "entries found: %s" % (c, str(duplicates)))
--- src/vfb/curation/test/test_peevish.py
+++ src/vfb/curation/test/test_peevish.py
@@ -1,11 +1,19 @@
 import unittest
-from ..peevish import Record
+from ..peevish import get_recs
+import os
 
 
 class Test_tsv_headers(unittest.TestCase):
-        def setUp(self):\
-            #Stub
-            return
+
+    def test_get_recs(self):
+        print(os.getcwd())
+        recs = get_recs(path_to_recs="src/vfb/curation/test/resources/records/new_images/",
+                 spec_path="records/")
+        for r in recs:
+            r.check_working()
+
+
+
 
 
 
