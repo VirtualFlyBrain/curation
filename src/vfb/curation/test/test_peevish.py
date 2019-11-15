c8fd46d6d2d11439bf9bdb7a8ca611f13e6e7135
dosumis <dosumis@gmail.com>
2019-11-15 11:11:36 +0000

    Test - adding main  method

--- src/vfb/curation/test/test_peevish.py
+++ src/vfb/curation/test/test_peevish.py
@@ -6,11 +6,15 @@ import os
 class Test_peevish(unittest.TestCase):
 
     def test_get_recs(self):
-        print(os.getcwd())
+        print("\n*** Running from: " + os.getcwd() + "\n\n")
         recs = get_recs(path_to_recs="src/vfb/curation/test/resources/records/new_images/",
                  spec_path="records/")
         for r in recs:
             r.check_working()
+
+if __name__ == '__main__':
+    unittest.main()
+
 
 
 
